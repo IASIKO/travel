c33ffb6c96e9a56c93bc52d3ebee6b3fd9437a7d
Giorgi Iaseshvili <iaseshviligi@gmail.com>
2023-04-05 18:55:11 +0400

    add

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import "./home.css";
 import video from "../../Assets/video.mp4";
 import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
+
 
 const Home = () => {
   return (
@@ -37,8 +44,26 @@ const Home = () => {
               <h3 className="total">$5000</h3>
             </div>
             <div className="input flex">
-              <input type="range" max='5000' min='1000' />
+              <input type="range" max="5000" min="1000" />
             </div>
+          </div>
+
+          <div className="searchOptions flex">
+            <HiFilter className="icon" />
+            <span>MORE FILTERS</span>
+          </div>
+        </div>
+
+        <div className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook className="icon" />
+            <AiOutlineInstagram className="icon" />
+            <FaTripadvisor className="icon" />
+          </div>
+
+          <div className="leftIcons">
+            <BsListTask className="icon" />
+            <TbApps className="icon" />
           </div>
         </div>
       </div>
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,162 @@
-import React from 'react'
-import './main.css'
+import React from "react";
+import "./main.css";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
+import img from "../../Assets/img.jpg";
+import img2 from "../../Assets/img2.jpg";
+import img3 from "../../Assets/img3.jpg";
+import img4 from "../../Assets/img4.jpg";
+import img5 from "../../Assets/img5.jpg";
+import img6 from "../../Assets/img6.jpg";
+import img7 from "../../Assets/img7.jpg";
+import img8 from "../../Assets/img8.jpg";
+import img9 from "../../Assets/img9.jpg";
+
+
+
+const Data = [
+  {
+    id: 1,
+    imgSrc: img,
+    destTitle: "Bora Bora",
+    location: "New Zealand",
+    grade: "CULTURAL RELAX",
+    fees: "$700",
+    desciption:
+      "The opitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxorious stays and adventurous activities.",
+  },
+
+  {
+    id: 2,
+    imgSrc: img2,
+    destTitle: 'Machu Picchu',
+    location: 'Peru',
+    grade: 'CULTURAL RELAX',
+    fees: '$600',
+    desciption: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quae doloribus consequatur, aperiam voluptas consectetur eaque illum perferendis sint facere.'
+  },
+
+  {
+    id: 3,
+    imgSrc: img3,
+    destTitle: 'Greet Barrier Reef',
+    location: 'Australia',
+    grade: 'CULTURAL RELAX',
+    fees: '$700',
+    desciption: 'The opitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxorious stays and adventurous activities.'
+  },
+
+  {
+    id: 4,
+    imgSrc: img4,
+    destTitle: 'Cappadocia',
+    location: 'Turkey',
+    grade: 'CULTURAL RELAX',
+    fees: '$400',
+    desciption: 'The opitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxorious stays and adventurous activities.'
+  },
+
+  {
+    id: 5,
+    imgSrc: img5,
+    destTitle: 'Guanajuato',
+    location: 'Mexico',
+    grade: 'CULTURAL RELAX',
+    fees: '$1100',
+    desciption: 'The opitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxorious stays and adventurous activities.'
+  },
+
+  {
+    id: 6,
+    imgSrc: img6,
+    destTitle: 'Cinque Terre',
+    location: 'Italy',
+    grade: 'CULTURAL RELAX',
+    fees: '$840',
+    desciption: 'The opitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxorious stays and adventurous activities.'
+  },
+
+  {
+    id: 7,
+    imgSrc: img7,
+    destTitle: 'Angkor Wat',
+    location: 'Cambodia',
+    grade: 'CULTURAL RELAX',
+    fees: '$650',
+    desciption: 'The opitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxorious stays and adventurous activities.'
+  },
+
+  {
+    id: 8,
+    imgSrc: img8,
+    destTitle: 'Taj Mahal',
+    location: 'India',
+    grade: 'CULTURAL RELAX',
+    fees: '$900',
+    desciption: 'The opitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxorious stays and adventurous activities.'
+  },
+
+  {
+    id: 9,
+    imgSrc: img9,
+    destTitle: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'CULTURAL RELAX',
+    fees: '$500',
+    desciption: 'The opitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxorious stays and adventurous activities.'
+  },
+];
 
 const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 className="title">Most visited destinations</h3>
+      </div>
 
-export default Main
+      <div className="secContent grid">
+        {Data.map(
+          ({ id, imgSrc, destTitle, location, grade, fees, desciption }) => {
+            return (
+              <div key={id} className="singleDestination">
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className="icon" />
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>
+                        {grade}
+                        <small> +1</small>
+                      </span>
+                    </div>
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+
+                  <div className="desc">
+                    <p>{desciption}</p>
+                  </div>
+
+                  <button className="btn flex">
+                    DETAILS <HiOutlineClipboardCheck className="icon" />
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Main;
