7f75a486b6596ef080a1b7328cf8b8dac6673775
Giorgi Iaseshvili <iaseshviligi@gmail.com>
2023-04-06 16:37:51 +0400

    add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './footer.css'
+import video2 from '../../Assets/video2.mp4'
+import {FiSend} from 'react-icons/fi'
+import {MdOutlineTravelExplore} from 'react-icons/md'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='footer'>
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder='Enter Email Address'/>
+            <button className='btn flex' type='submit'>
+              SEND <FiSend className='icon'/>
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className='logo flex'>
+                <MdOutlineTravelExplore className='icon'/> Travel.
+              </a>
+            </div>
+
+            <div className="footerParagraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, laudantium esse! Distinctio dignissimos quia sequi, dolore porro excepturi vel! Quod, numquam nulla dignissimos fugit et optio eius provident! Amet, saepe?
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
