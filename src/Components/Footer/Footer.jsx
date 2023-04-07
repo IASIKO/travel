e2bb033d3a42d662306622c0a8634c874e7cc4e8
Giorgi Iaseshvili <iaseshviligi@gmail.com>
2023-04-07 14:07:25 +0400

    add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './footer.css'
 import video2 from '../../Assets/video2.mp4'
 import {FiSend} from 'react-icons/fi'
@@ -8,8 +8,20 @@ import {AiFillYoutube} from 'react-icons/ai'
 import {AiFillInstagram} from 'react-icons/ai'
 import {SiTripadvisor} from 'react-icons/si'
 import {FiChevronRight} from 'react-icons/fi'
+import {CgCopyright} from 'react-icons/cg'
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Footer = () => {
+
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  },[])
+
+
+
+
   return (
     <section className='footer'>
       <div className="videoDiv">
@@ -18,14 +30,14 @@ const Footer = () => {
 
       <div className="secContent container">
         <div className="contactDiv flex">
-          <div className="text">
+          <div data-aos='fade-up' className="text">
             <small>KEEP IN TOUCH</small>
             <h2>Travel with us</h2>
           </div>
 
           <div className="inputDiv flex">
-            <input type="text" placeholder='Enter Email Address'/>
-            <button className='btn flex' type='submit'>
+            <input data-aos='fade-up' type="text" placeholder='Enter Email Address'/>
+            <button data-aos='fade-up' className='btn flex' type='submit'>
               SEND <FiSend className='icon'/>
             </button>
           </div>
@@ -39,10 +51,10 @@ const Footer = () => {
               </a>
             </div>
 
-            <div className="footerParagraph">
+            <div data-aos='fade-up' className="footerParagraph">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, laudantium esse! Distinctio dignissimos quia sequi, dolore porro excepturi vel! Quod, numquam nulla dignissimos fugit et optio eius provident! Amet, saepe?
 
-              <div className="footerSocials flex">
+              <div data-aos='fade-up' className="footerSocials flex">
                 <AiOutlineTwitter className='icon'/>
                 <AiFillYoutube className='icon'/>
                 <AiFillInstagram className='icon'/>
@@ -52,7 +64,8 @@ const Footer = () => {
           </div>
 
           <div className="footerLinks grid">
-            <div className="linkGroup">
+            {/* group 1 */}
+            <div data-aos='fade-up' data-aos-duration='3000' className="linkGroup">
               <span className="groupTitle">
                 OUR AGENCY
               </span>
@@ -83,6 +96,82 @@ const Footer = () => {
               </li>
 
             </div>
+
+            {/* group 2 */}
+
+            <div data-aos='fade-up' data-aos-duration='4000' className="linkGroup">
+              <span className="groupTitle">
+                PARTNERS
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Bookings
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Rentcars
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                HostelWorld
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Trivago
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                TripAdvaisor
+              </li>
+
+            </div>
+
+            {/* group 3 */}
+
+            <div data-aos='fade-up' data-aos-duration='5000' className="linkGroup">
+              <span className="groupTitle">
+                LAST MINUTE
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                London
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                California
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Indonesia
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Europe
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className='icon'/>
+                Oceania
+              </li>
+
+            </div>
+
+            
+          </div>
+
+          <div className="footerDiv flex">
+              <small>BEST TRAVEL WEBSITE THEME</small>
+              <small>COPYRIGHT <CgCopyright/> - IASIKO 2023</small>
+
           </div>
         </div>
       </div>
